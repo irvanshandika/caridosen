fb082e26af1530e97c62985624c6eb5e0afe7312
irvanshandika <shandikamuhammadirvan@gmail>
2024-05-17 20:29:05 +0700

    Add alert when name lecturer didn't find

--- src/pages/Dosens.tsx
+++ src/pages/Dosens.tsx
@@ -5,11 +5,14 @@ import { Helmet } from "react-helmet";
 import { db } from "@config/FirebaseConfig";
 import { collection, getDocs } from "@firebase/firestore";
 import { useSearchParams, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 function Dosens() {
   const [Data, setData] = useState<any[]>([]);
   const [filteredData, setFilteredData] = useState<any[]>([]);
   const [search, setSearch] = useState("");
+  // const [notFoundMessage, setNotFoundMessage] = useState("");
+
   const navigate = useNavigate();
 
   const [searchParams] = useSearchParams();
@@ -37,10 +40,17 @@ function Dosens() {
   useEffect(() => {
     const searchQuery = searchParams.get("search")?.toLowerCase() || "";
     setSearch(searchQuery);
-    const filtered = Data.filter((dosen) =>
-      dosen.nama.toLowerCase().includes(searchQuery)
-    );
+    const filtered = Data.filter((dosen) => dosen.nama.toLowerCase().includes(searchQuery));
     setFilteredData(filtered);
+
+    if (filtered.length === 0 && searchQuery) {
+      Swal.fire({
+        title: "Tidak Ditemukan",
+        text: `Nama Dosen "${searchParams.get("search")}" Tidak Ditemukan`,
+        icon: "error",
+        confirmButtonText: "OK",
+      });
+    }
   }, [searchParams, Data]);
 
   return (
@@ -51,30 +61,18 @@ function Dosens() {
       <div className="flex flex-col justify-center items-center h-screen">
         <form onSubmit={handleSearch}>
           <div className="flex">
-            <input
-              type="text"
-              value={search}
-              onChange={(e: any) => setSearch(e.target.value)}
-              placeholder="Cari Dosen Kalian"
-              className="border-[#003566] border-2 rounded-[8px] w-72 bg-[#DFF6FF] placeholder:text-[#003566]"
-            />
-            <button
-              type="submit"
-              className="bg-[#003566] text-white px-5 py-2 rounded-lg ml-2"
-            >
+            <input type="text" value={search} onChange={(e: any) => setSearch(e.target.value)} placeholder="Cari Dosen Kalian" className="border-[#003566] border-2 rounded-[8px] w-72 bg-[#DFF6FF] placeholder:text-[#003566]" />
+            <button type="submit" className="bg-[#003566] text-white px-5 py-2 rounded-lg ml-2">
               Cari
             </button>
           </div>
         </form>
         <div className="flex flex-wrap justify-center">
+          {/* {notFoundMessage && <div className="text-center text-red-500 mt-4">{notFoundMessage}</div>} */}
           {filteredData.map((data: any) => (
             <div key={data.id} className="m-4">
               <div className="flex flex-col items-center">
-                <img
-                  src={data.urlFoto}
-                  alt={data.nama}
-                  className="w-40 h-40 rounded-full"
-                />
+                <img src={data.urlFoto} alt={data.nama} className="w-40 h-40 rounded-full" />
                 <h1 className="text-center">{data.nama}</h1>
                 <h2 className="text-center">{data.nip}</h2>
                 <h3 className="text-center">{data.email}</h3>
