c4b29558ff88996be6f2ddbb14d71882241017d9
irvanshandika <shandikamuhammadirvan@gmail>
2024-05-30 12:43:26 +0700

    Fix photo profile teams and name

--- src/data/TeamData.ts
+++ src/data/TeamData.ts
@@ -12,22 +12,22 @@ const teamMembers: TeamMember[] = [
       "https://scontent.fjog3-1.fna.fbcdn.net/v/t39.30808-6/430977706_1987487408348980_3894309208797817253_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeG86ngWp2B9iUHTlLKkk10cXOA8-gkITAVc4Dz6CQhMBcLlx1tWF5iMyTdqDiDI8Q5M3goUdbu0rpzfRuTTnWG-&_nc_ohc=gs4xUr5Kg7gQ7kNvgHDPIoX&_nc_ht=scontent.fjog3-1.fna&oh=00_AYAzGo9MQqkkS3tHqd8T9RizcU9svBszHgeUcyalHtD9xg&oe=665A2DB3",
   },
   {
-    name: "Alfa",
+    name: "Alfa Zidan Prastyo",
     role: "UI/UX Designer",
     imgSrc: "https://lets-votely.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FAlfa.f7c177e9.png&w=640&q=75",
   },
   {
     name: "Aryawijaya",
     role: "Scrum Master",
-    imgSrc: "https://drive.google.com/file/d/1iW4QExuhUXsPXUQ4S270ItMJ9tjCsn8a/view?usp=drive_link",
+    imgSrc: "https://res.cloudinary.com/dszhlpm81/image/upload/v1717047636/assets/caridosen/DSC01993_yshwtr.jpg",
   },
   {
     name: "Raffa Nur Listiawan D.E.S",
     role: "Business Analyst",
-    imgSrc: "https://lets-votely.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffirman.6c1b4d4f.png&w=640&q=75",
+    imgSrc: "https://res.cloudinary.com/dszhlpm81/image/upload/v1717047597/assets/caridosen/RapoL_m9cmxd.jpg",
   },
   {
-    name: "Fauzan Lutfi Muzaki",
+    name: "Fauzan",
     role: "Product Owner",
     imgSrc: "https://lets-votely.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffirman.6c1b4d4f.png&w=640&q=75",
   },
