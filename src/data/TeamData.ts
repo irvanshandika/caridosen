bea08945f0206fb7b7f9a171bd9edcc688680cf3
Muhammad Taqiyuddin <mtaqiyuddin101@gmail.com>
2024-05-30 20:42:17 +0700

    Edit Profil

--- src/data/TeamData.ts
+++ src/data/TeamData.ts
@@ -14,27 +14,32 @@ const teamMembers: TeamMember[] = [
   {
     name: "Alfa Zidan Prastyo",
     role: "UI/UX Designer",
-    imgSrc: "https://lets-votely.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FAlfa.f7c177e9.png&w=640&q=75",
+    imgSrc:
+      "https://lets-votely.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2FAlfa.f7c177e9.png&w=640&q=75",
   },
   {
     name: "Aryawijaya",
     role: "Scrum Master",
-    imgSrc: "https://res.cloudinary.com/dszhlpm81/image/upload/v1717047636/assets/caridosen/DSC01993_yshwtr.jpg",
+    imgSrc:
+      "https://res.cloudinary.com/dszhlpm81/image/upload/v1717047636/assets/caridosen/DSC01993_yshwtr.jpg",
   },
   {
     name: "Raffa Nur Listiawan D.E.S",
     role: "Business Analyst",
-    imgSrc: "https://res.cloudinary.com/dszhlpm81/image/upload/v1717047597/assets/caridosen/RapoL_m9cmxd.jpg",
+    imgSrc:
+      "https://res.cloudinary.com/dszhlpm81/image/upload/v1717047597/assets/caridosen/RapoL_m9cmxd.jpg",
   },
   {
     name: "Fauzan Lutfi Muzaki",
     role: "Product Owner",
-    imgSrc: "https://res.cloudinary.com/dszhlpm81/image/upload/v1717073722/assets/caridosen/Fauzan_ozpddo.png",
+    imgSrc:
+      "https://res.cloudinary.com/dszhlpm81/image/upload/v1717073722/assets/caridosen/Fauzan_ozpddo.png",
   },
   {
     name: "Muhammad Taqiyuddin",
-    role: "Frontend Developer",
-    imgSrc: "https://lets-votely.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ffirman.6c1b4d4f.png&w=640&q=75",
+    role: "Quality Assurance",
+    imgSrc:
+      "https://res.cloudinary.com/dszhlpm81/image/upload/v1717047597/assets/caridosen/Taqi.jpg",
   },
 ];
 
